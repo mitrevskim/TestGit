2f7e47b73e1e3e7ac3932b8965c4ec24719ee2e7
mitrevskim <mitrevskimile@gmail.com>
2022-04-20 22:23:49 +0200

    First commit

--- src/com/company/Testclass.java
+++ src/com/company/Testclass.java
@@ -3,5 +3,9 @@ package com.company;
 public class Testclass {
     public static void main(String[] args) {
         System.out.println("Hello team");
+        System.out.println("Homework");
+        System.out.println("testing");
+
+
     }
 }
